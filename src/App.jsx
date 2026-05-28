23dda809c07417d1d2a1761adae93af36c9cbf48
NA <rushikeshgaikwad2565@gmail.com>
2026-05-28 11:09:14 +0530

    stages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+/*git init
+git add .
+git commit -m "message"
+git branch -M main
+git remote add origin URL
+git push -u origin main*/
 import React from "react";
 import Person from './Person'
 import Navbar from './components/Navbar'
